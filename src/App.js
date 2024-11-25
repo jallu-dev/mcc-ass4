182de9c33e442b63f3c7f397801aea3bcefc47f0
Liyasdeen Mohammed Zahran <zzzahrannnldeen@gmail.com>
2024-11-25 22:13:28 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Result from "./components/Result";
+import Animal from "./components/Animal";
+import AnimalsBox from "./components/AnimalsBox";
+import { animals } from "./data/AnimalsDb";
+
+const selectRandomAnimal = (arr) => {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+
+  return arr[randomIndex].name;
+};
 
 function App() {
+  const [result, setResult] = useState("");
+  const [randomAnimal, setRandomAnimal] = useState(selectRandomAnimal());
+  const [selectedAnimal, setSelectedAnimal] = useState("");
+
+  const checkResult = (selectedAnimal) => {
+    if (selectedAnimal === randomAnimal) {
+      setResult("Win!");
+
+      setTimeout(() => {
+        setResult("");
+        setRandomAnimal(selectRandomAnimal(animals));
+      }, 5000);
+    } else {
+      setResult("Lose!");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="border-orange">
+        <h1>Animal Matching Game</h1>
       </header>
+
+      <div className="board">
+        <Result result={result} />
+
+        <Animal
+          animals={animals}
+          randomAnimal={randomAnimal}
+          setRandomAnimal={setRandomAnimal}
+        />
+
+        <AnimalsBox
+          animals={animals}
+          checkResult={checkResult}
+          selectedAnimal={selectedAnimal}
+          setSelectedAnimal={setSelectedAnimal}
+        />
+      </div>
     </div>
   );
 }
