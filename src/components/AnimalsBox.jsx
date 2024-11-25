5a576de873000e1e3e5a1db75b767a435a581a48
Liyasdeen Mohammed Zahran <zzzahrannnldeen@gmail.com>
2024-11-25 21:43:11 +0530

    AnimalBox created

---
+++ src/components/AnimalsBox.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const AnimalsBox = ({
+  animals,
+  checkResult,
+  selectedAnimal,
+  setSelectedAnimal,
+}) => {
+  return (
+    <div className="border-orange">
+      <h3>Choose an Animal</h3>
+      <div className="animal-grid">
+        {animals.map((animal, index) => (
+          <div
+            key={index}
+            className="animal-card"
+            onClick={() => {
+              setSelectedAnimal(animal.name);
+              checkResult(animal.name);
+            }}
+          >
+            <img
+              className={`animal-image ${
+                selectedAnimal === animal.name ? "selected" : ""
+              }`}
+              src={`./fig/${animal.img}`}
+              alt={animal.name}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AnimalsBox;
